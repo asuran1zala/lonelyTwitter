bac3b4559d83ba5c52f50bffedf51082798ad07e
asuran1zala <wkchoi@ualberta.ca>
2014-01-16 16:44:17 -0700

    changes made

--- src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
+++ src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class TweetListModel
 {
-	private static ArrayList<LonelyTweetModel> tweetList;
+	private static ArrayList<AbstractTweetModel> tweetList;
 
 	
-	public ArrayList<LonelyTweetModel> getTweetList()
+	public ArrayList<AbstractTweetModel> getTweetList()
 	{
 		return tweetList;
 	}
 
 	
-	public void setTweetList(ArrayList<LonelyTweetModel> tweetList)
+	public void setTweetList(ArrayList<AbstractTweetModel> tweetList)
 	{
 		this.tweetList = tweetList;
 	}
@@ -22,7 +22,7 @@ public class TweetListModel
 	public TweetListModel()
 	{
 		super();
-		tweetList = new ArrayList<LonelyTweetModel>();
+		tweetList = new ArrayList<AbstractTweetModel>();
 	}
 	
 	
